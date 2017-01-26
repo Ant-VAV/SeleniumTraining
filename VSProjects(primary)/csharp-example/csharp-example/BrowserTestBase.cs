38f9bec5247740533009a9e5ed82311d72fc9b2d
Anton Vdovichenko <ant-vav@ya.ru>
2017-01-26 22:21:46 +0500

    Занятие 2, задание 3

---
+++ VSProjects(primary)/csharp-example/csharp-example/BrowserTestBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace csharp_example
+{
+    public class BrowserTestBase
+    {
+        protected IWebDriver Driver;
+        protected WebDriverWait Wait;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Driver = new ChromeDriver();
+            Wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+            Driver = null;
+        }
+    }
+}
---
+++ VSProjects(primary)/csharp-example/csharp-example/TestClass.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace csharp_example
+{
+    /// <summary>
+    ///     Задание http://software-testing.ru/lms/mod/assign/view.php?id=38582
+    /// </summary>
+    public class TestClass : BrowserTestBase
+    {
+        private readonly string namePassword = "admin";
+        private readonly string baseUrl = @"http://localhost/litecart/admin/";
+
+        [Test]
+        public void LoginToAdmintools()
+        {
+            Driver.Url = baseUrl;
+
+            var userName = Driver.FindElement(By.Name("username"));
+            userName.Clear();
+            userName.SendKeys(namePassword);
+
+            var passwor = Driver.FindElement(By.Name("password"));
+            passwor.Clear();
+            passwor.SendKeys(namePassword);
+
+            Driver.FindElement(By.CssSelector(".footer button")).Click();
+        }
+    }
+}
